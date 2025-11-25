33ca3dd9f1b8e885e0dddb636538fb08b28aeccf
Logan Farci <farci.logan@gmail.com>
2025-11-25 15:29:51 +0100

    Remove access policy for admin users from Key Vault configuration

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -9,21 +9,8 @@ resource "azurerm_key_vault" "main" {
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
+  rbac_authorization_enabled = true
   tags                       = var.tags
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Purge",
-      "Recover"
-    ]
-  }
 }
 
 # Store private SSH key as a secret
@@ -34,17 +21,4 @@ resource "azurerm_key_vault_secret" "ssh_private_key" {
   tags         = var.tags
 
   depends_on = [azurerm_key_vault.main]
-}
-
-# Access policy for admin users
-resource "azurerm_key_vault_access_policy" "admin_users" {
-  for_each     = toset(var.admin_users)
-  key_vault_id = azurerm_key_vault.main.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = each.value
-
-  secret_permissions = [
-    "Get",
-    "List"
-  ]
 }
